4c5ce02a946e9b7178427632586e1f3c3925d426
rafacortts <rafaelcorttes1@gmail.com>
2024-06-05 16:10:21 -0300

    BUG01

--- TelaSuporte/processar_formulario.php
+++ TelaSuporte/processar_formulario.php
@@ -5,12 +5,11 @@ $username = "root";
 $password = "";
 $dbname = "cadastrophp";
 
-
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Verificar a conexão
 if ($conn->connect_error) {
-    		die("Conexão falhou: " . $conn->connect_error);
+    die("Conexão falhou: " . $conn->connect_error);
 }
 
 // Capturar dados do formulário
@@ -18,18 +17,22 @@ $nome = $_POST['nome'];
 $email = $_POST['email'];
 $cpf = $_POST['cpf'];
 
-      $sql = "INSERT INTO cliente (nome, email, cpf) VALUES ('$nome', '$email', $cpf)";
-
-
+$sql = "INSERT INTO cliente (nome, email, cpf) VALUES ('$nome', '$email', '$cpf')";
 
 if ($conn->query($sql) === TRUE) {
-    		echo "Registro inserido com sucesso!";
+    echo "<script>
+            alert('Registro inserido com sucesso!');
+            window.location.href = '../index.html';
+          </script>";
 } else {
-    		echo "Erro ao inserir registro: " . $conn->error;
+    echo "Erro ao inserir registro: " . $conn->error;
 }
 
 $conn->close();
 
-header("Location: ../index.html");
-exit();
+$stmt = $conn->prepare("INSERT INTO cliente (nome, email, cpf) VALUES (?, ?, ?)");
+$stmt->bind_param("sss", $nome, $email, $cpf);
+
+
 ?>
+
